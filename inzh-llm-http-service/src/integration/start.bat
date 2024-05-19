b3f635a16819d8d914e3040eb4ad892d43539544
Jean-Raffi Nazareth <jr-nazareth@inzh.fr>
2024-05-19 14:21:39 +0200

    Upgrade version 0.1 -> 0.2

--- inzh-llm-http-service/src/integration/start.bat
+++ inzh-llm-http-service/src/integration/start.bat
@@ -9,7 +9,7 @@ set root=%~dp0
 set root=%root:~0,-1%
 
 set native=%root%\lib\native
-set jar=%root%\inzh-llm-http-service-0.1.jar
+set jar=%root%\inzh-llm-http-service-0.2.jar
 set PATH=%native%;%PATH%
 
 "%JAVA_HOME%\bin\java.exe" -Djava.library.path="%native%" -jar "%jar%" --home "%root%" %*
